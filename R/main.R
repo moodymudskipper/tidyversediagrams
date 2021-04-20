97db77f6a9957921866533bfbb6c0fe43d23ac06
Antoine Fabri <antoine.fabri@gmail.com>
2021-04-20 15:37:34 +0200

    Initial commit

---
+++ R/main.R
@@ -0,0 +1,9 @@
+
+  # pkgs <- c(
+  #   "ggplot2", "dplyr", "tidyr", "readr",
+  #   "purrr", "tibble", "stringr", "forcats")
+  # for (pkg in pkgs) {
+  #   flow::flow_doc(pkg, out = paste0(pkg, ".md"), engine = "plantuml")
+  # }
+  # usethis::use_pkgdown()
+  # pkgdown::build_site()
